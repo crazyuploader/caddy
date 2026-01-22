92eb89cbb2c19c1e245da24e733d1c29ed43c7fb
Jugal Kishore <me@devjugal.com>
2026-01-23 00:45:05 +0530

    Update script

--- scripts/preremove.sh
+++ scripts/preremove.sh
@@ -4,4 +4,6 @@ set -e
 if [ -d /run/systemd/system ] && [ "$1" = "remove" ]; then
     systemctl --no-reload disable caddy >/dev/null 2>&1 || true
     systemctl stop caddy >/dev/null 2>&1 || true
+    systemctl --no-reload disable caddy-api >/dev/null 2>&1 || true
+    systemctl stop caddy-api >/dev/null 2>&1 || true
 fi
